dc1179327a9c0f66639167625df8a69ae0d32547
whimy <114148910+whimy@users.noreply.github.com>
2022-12-13 11:55:25 +0800

    Add rules detect

--- inc/goma.h
+++ inc/goma.h
@@ -1,4 +1,4 @@
-﻿#ifndef __CHESS_H__
+#ifndef __CHESS_H__
 #define __CHESS_H__
 
 #define print_xgoma(x) printf("\033[31m%s\033[m ",x)
@@ -15,7 +15,7 @@
 #define BISHOP "角"
 #define EMPTY "　"
 
-typedef int (*func)(char*);
+typedef int (*func)(char,int*,int*,char);
 
 typedef struct goma_t{
     char *syb;
@@ -29,10 +29,25 @@ typedef struct bag_t{
     int top;
 }BAG;
 
+typedef enum Direction_t
+{
+    FORWARD,BACKWARD,LEFT,RIGHT,FL,FR,BL,BR
+}Direction;
+
 GOMA *Pawn, *Lance, *Knight, *Sliver, *Gold, *King, *Rook, *Bishop, *Empty;
 BAG *x_bag, *y_bag;
 
 void generate_goma();
 BAG* init_bag(BAG* ptbag);
+Direction detect_direction(char user, int dx,int dy);
+int detect_Pawn(char user,int* px,int* py, char detect_mode);
+int detect_Lance(char user,int* px,int* py, char detect_mode);
+int detect_Knight(char user,int* px,int* py, char detect_mode);
+int detect_Sliver(char user,int* px,int* py, char detect_mode);
+int detect_Gold(char user,int* px,int* py, char detect_mode);
+int detect_King(char user,int* px,int* py, char detect_mode);
+int detect_Rook(char user,int* px,int* py, char detect_mode);
+int detect_Bishop(char user,int* px,int* py, char detect_mode);
+int detect_path(int* px,int *py);
 
 #endif
