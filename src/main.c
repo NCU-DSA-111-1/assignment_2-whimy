b59a6930495b76e828ce001192cad9bf523d7a9f
whimy <114148910+whimy@users.noreply.github.com>
2022-12-21 17:37:21 +0800

    Build basic Libev func.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <ev.h>
 #include "backprop.h"
 #include "board.h"
 #include "goma.h"
@@ -15,9 +16,15 @@ FILE *fp = NULL;
 NODE* list = NULL;
 char *condition = NULL;
 int regret;
+int player = X;
+ev_io stdin_watcher;
+ev_timer timeout_watcherX;
+ev_timer timeout_watcherY;
+char *name = NULL;
 
 int main(int argc, char *argv[]) {
     // detect which program to execute
+    name = argv[3]; 
     while((para = getopt(argc, argv, "nsl:")) != -1) 
     { 
         switch(para) 
@@ -31,7 +38,7 @@ int main(int argc, char *argv[]) {
             case 'l':
                 printf("[Reload old game]\n");
                 Reload_old_game(fp,argv[2]);
-                // `read_board
+                // read_board
                 break;
             case '?':
                 printf("Unknown parameter.\n");
@@ -47,7 +54,10 @@ void game_start(char *gname){
     // initial setting
     char result = NA;
     int turns = 1;
-    int player = X;
+    struct ev_loop *loop = EV_DEFAULT;
+    ev_io_init(&stdin_watcher, stdin_cb, 0, EV_READ);
+    ev_timer_init(&timeout_watcherX,timeout_cbX, 3, 0);
+    ev_timer_init(&timeout_watcherY,timeout_cbY, 3, 0);
 
     // init_board
     printf("Initialize board...\n\n");
@@ -64,15 +74,18 @@ void game_start(char *gname){
     x_bag = init_bag(x_bag);
     y_bag = init_bag(y_bag);
 
-
-    // initialize board
-    printf("Initialize board...\n\n");
     init_board(ptboard);
     visualize_board(ptboard);
 
     do{
+        ev_io_start(loop, &stdin_watcher);
         printf("[Turn%d]\n",turns);
         printf("[Player %c]\n",(player)? 'y':'x');
+        if(!player){
+            ev_timer_start(loop, &timeout_watcherX);
+        }else{
+            ev_timer_start(loop, &timeout_watcherY);
+        }
         // regret?
         if(turns > 2){
             printf("Would you like to regret?[0/1]:");
@@ -86,8 +99,12 @@ void game_start(char *gname){
                 continue;
             }
         }
-        goma_move(player,gname);
+
+        goma_move(gname);
+        ev_run(loop, 0);
         visualize_board(ptboard);
+
+
         if(list->new_pos.goma == King){
             if(list->new_pos.owner == 'y'){
                 printf("Player x win!\n");
@@ -110,7 +127,7 @@ void game_start(char *gname){
 
 }
 
-void goma_move(int user,char *gname){
+void goma_move(char *gname){
     int *ptx = (int*)malloc(2*sizeof(int));
     int *pty = (int*)malloc(2*sizeof(int));
     BOARD *pre_select = (BOARD*)malloc(sizeof(BOARD));
@@ -120,8 +137,8 @@ void goma_move(int user,char *gname){
     do{
         do{   
             printf("Please enter the initial position:");
-            scanf(" %d",ptx);
-            scanf(" %d",pty);
+            scanf("%d",ptx);
+            scanf("%d",pty);
             pre_select = &ptboard[*pty - 1][ROW - *ptx];
             if(*ptx <= 0 || *pty <= 0 || *ptx > ROW || *pty > COLUMN){
                 printf("\n\033[31m[Error 1]:This position does not exist.\033[m\nPlease try again.\n\n");
@@ -129,7 +146,7 @@ void goma_move(int user,char *gname){
             else if(pre_select->goma == NULL){
                 printf("\n\033[31m[Error 1]:There is empty.\033[m\nPlease try again.\n\n");
             }
-            else if(pre_select->owner != ((user)? 'y':'x')){
+            else if(pre_select->owner != ((player)? 'y':'x')){
                 printf("\n\033[31m[Error 2]This is not your chess.\033[m\nPlease try again.\n\n");
             }else{
                 printf("Select ");
@@ -202,23 +219,21 @@ void goma_move(int user,char *gname){
 
     }while(pre_select == NULL || new_select == NULL || *condition == 'y');
     list = Add_to_list(list,ptx,pty);
-    write_file(fp, gname, ptx, pty);
+    write_file(fp, gname, list, ptx, pty);
     swap(pre_select,new_select);
-    printf("%s move from [%d][%d] to [%d][%d]\n",list->old_pos.goma->syb,*(list->cx),*(list->cy),*(list->cx+1),*(list->cy+1));
-    // print_step(new_select,*ptx,*pty);
-}
+    printf("%s move from [%d][%d] to [%d][%d]\n", list->old_pos.goma->syb, list->cx[0], list->cy[0], list->cx[1], list->cy[1]);
 
+    printf("\nWould you want to leave the game?[s/n]");
+    scanf(" %c", condition);
+    if(*condition == 's' || *condition == 'S'){
+        printf("Saving...\n\n");
+        printf("Saving completed.\nGoodbye.\n");
+        exit(EXIT_SUCCESS);
+    }
+}
 
 void free_mem(){
     free(ptboard);
-    // free(PAWN);
-    // free(LANCE);
-    // free(KNIGHT);
-    // free(SLIVER);
-    // free(GOLD);
-    // free(KING);
-    // free(ROOK);
-    // free(BISHOP);
 }
 
 void print_step(BOARD* sel, int x, int y){
@@ -257,3 +272,33 @@ void print_step(BOARD* sel, int x, int y){
     printf("============================\n");
 }
     
+
+static void timeout_cbX(EV_P_ ev_timer *w, int revents){
+    printf("Total time:%d\n",(int)ev_now(loop));
+    // ev_break(EV_A_ EVBREAK_ONE);
+    ev_timer_stop(loop, &timeout_watcherX);
+    ev_timer_start(loop, &timeout_watcherY);
+    ev_break(EV_A_ EVBREAK_ONE);
+}
+
+static void timeout_cbY(EV_P_ ev_timer *w, int revents){
+    printf("Total time:%d\n",(int)ev_now(loop));
+    // 
+    ev_timer_stop(loop, &timeout_watcherY);
+    ev_timer_start(loop, &timeout_watcherX);
+    ev_break(EV_A_ EVBREAK_ONE);
+}
+
+static void stdin_cb(EV_P_ ev_io *w, int revents){
+    extern char *name;
+    ev_timer_stop(loop, &timeout_watcherX);
+    ev_timer_stop(loop, &timeout_watcherY);
+    
+    if(player){
+        ev_timer_stop(loop, &timeout_watcherX);
+        ev_timer_start(loop, &timeout_watcherY);
+    }else{
+        ev_timer_stop(loop, &timeout_watcherY);
+        ev_timer_start(loop, &timeout_watcherX); 
+    }
+}
