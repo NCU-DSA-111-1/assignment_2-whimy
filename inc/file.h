51e89f88362188ce64f413bd0adaf24bb6c6cf3a
whimy <114148910+whimy@users.noreply.github.com>
2022-12-21 17:40:37 +0800

    Renew struct node

--- inc/file.h
+++ inc/file.h
@@ -8,16 +8,18 @@
 struct node{
     BOARD old_pos;
     BOARD new_pos;
-    int* cx;
-    int* cy;
+    int cx[2];
+    int cy[2];
     struct node *next;
 };
 typedef struct node NODE;
+
 
 void Open_new_game(FILE* fp,char *fname);
 void Reload_old_game(FILE* fp,char *fname);
 NODE* Add_to_list(NODE* head, int *px, int *py);
 NODE* Remove_from_list(NODE* head, BAG *ptbag);
-void write_file(FILE* fp, char *fname, int* px, int* py);
+void write_file(FILE* fp, char *fname, NODE *head, int* px, int* py);
+void read_file(FILE* fp,char *fname);
 
 #endif
