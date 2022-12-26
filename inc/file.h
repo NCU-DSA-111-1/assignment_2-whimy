c739553b2894f998df4105db1c8307919ec78c15
whimy <114148910+whimy@users.noreply.github.com>
2022-12-26 17:51:51 +0800

    Update file.h

--- inc/file.h
+++ inc/file.h
@@ -15,10 +15,11 @@ struct node{
 typedef struct node NODE;
 
 
-void Open_new_game(FILE* fp,char *fname);
+FILE* Open_new_game(FILE* fp,char *fname);
 void Reload_old_game(FILE* fp,char *fname);
 NODE* Add_to_list(NODE* head, int *px, int *py);
 NODE* Remove_from_list(NODE* head, BAG *ptbag);
+void Remove_from_file(FILE *fp, char *fname);
 void write_file(FILE* fp, char *fname, NODE *head, int* px, int* py);
 void read_file(FILE* fp,char *fname);
 
